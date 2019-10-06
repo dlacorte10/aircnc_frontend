7d67201588ca92a693cc06ba6289a8ecd36e8ff6
Daniel Lacorte <daniel.lacorte10@gmail.com>
2019-10-05 22:42:26 -0300

    Adição do Logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+   <div  className="container">
+     <img src={logo} alt="AirCnC" />
+   </div>
   );
 }
 
