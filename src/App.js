fad63e9c7183bde1a2f2b8891aa1b4de8a8879aa
Daniel Lacorte <daniel.lacorte10@gmail.com>
2019-10-05 23:40:55 -0300

    Estilização do App.css

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ function App() {
   return (
    <div  className="container">
      <img src={logo} alt="AirCnC" />
+
+     <div className="content">
+      <p>
+        Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa      
+      </p>
+      <form>
+        <label htmlFor="email">E-MAIL *</label>
+        <input 
+          type="email" 
+          id="email" 
+          placeholder="Seu melhor email"
+        />
+
+        <button className="btn" type="submit">Entrar</button>
+      </form>
+     </div>
    </div>
   );
 }
